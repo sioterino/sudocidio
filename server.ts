71e6d43121c54e129f301cf3ffb75066748e2587
julliaturness <jmturnes10@gmail.com>
2026-04-13 16:36:34 -0300

    feat: add custom server, game types, and in-memory match engine

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
+import { randomUUID } from 'crypto';
+import { MatchState, BoardState } from './src/types/game';
+
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
@@ -11,8 +14,62 @@ const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 // este Map vai segurar o estado das partidas do sudocidio.
-const activeMatches = new Map();
+const activeMatches = new Map<String, MatchState>()
 
+// função para inicializar uma sala nova
+export function createMatch(): MatchState{
+  const matchId= randomUUID();
+
+  const initialBoard: BoardState = {
+    cells: [],
+    availablePieces:[]
+  }
+
+  const newMatch: MatchState = {
+    matchId,
+    players: [],
+    board: initialBoard,
+    hiddenAnswer: {
+      killerId: 'suspect_a',
+      weaponId: 'weapon_knife',
+      roomId: 'kitchen'
+    }
+  };
+  
+  activeMatches.set(matchId, newMatch);
+  console.log(`[Servidor] Nova partida criada: ${matchId}`);
+  
+  return newMatch;
+}
+
+//função para o jogador entrar na sala
+export function joinMatch(matchId: string, playerId: string): boolean{
+  const match = activeMatches.get(matchId);
+
+  if(!match){
+    console.error(`[Servidor] Erro: Partida ${matchId} não encontrada.`);  
+    return false;
+  }
+
+  if (match.players.length >= 2) {
+    console.error(`[Servidor] Erro: Partida ${matchId} já está cheia.`);
+    return false; 
+  }
+
+  match.players.push(playerId);
+  console.log(`[Servidor] Jogador ${playerId} entrou na partida ${matchId}`);
+  return true;
+}
+  
+// função para limpar a memória quando o jogo acabar
+export function removeMatch(matchId: string): void {
+  if (activeMatches.has(matchId)) {
+    activeMatches.delete(matchId);
+    console.log(`[Servidor] Partida ${matchId} encerrada e removida da memória.`);
+  }
+}
+
+// inicialização do servidor
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
